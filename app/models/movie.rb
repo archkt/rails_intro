d0056ee11e1cdfd9163f3871edb7f6558643d197
Ryan Min <archkt@berkeley.edu>
2021-10-14 10:22:14 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,21 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings()
+    ['G','PG','PG-13','R']
+  end
+  
+  def self.ratings_to_show(check_list)
+    puts(" ")
+    puts(" ")
+    puts(" ")
+    puts(" ")
+    puts(" ")
+    puts(check_list)
+    puts("hi")
+    puts(" ")
+    puts(" ")
+    puts(" ")
+    puts(" ")
+    puts(" ")
+  end
+    
 end
